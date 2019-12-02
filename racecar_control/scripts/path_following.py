d3a81bb7d082cf5804d5d41dee0c8a87d933d0c6
simonchamorro <simon.chamorro@usherbrooke.ca>
2019-12-02 14:51:01 -0500

    detect goal

--- racecar_control/scripts/path_following.py
+++ racecar_control/scripts/path_following.py
@@ -14,6 +14,8 @@ class PathFollowing:
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self.scan_sub = rospy.Subscriber('scan', LaserScan, self.scan_callback, queue_size=1)
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_callback, queue_size=1)
+        self.position = np.array([0, 0, 0])
+        self.goal = np.array([1, 1, 1])
 
     def idx_to_steering(self, idx, max_idx):
         forward = max_idx // 2
@@ -40,38 +42,46 @@ class PathFollowing:
         return steering
 
     def scan_callback(self, msg):
-        # Because the lidar is oriented backward on the racecar, 
-        # if we want the middle value of the ranges to be forward:
-        l2 = len(msg.ranges)/2;
-        ranges = msg.ranges[l2:len(msg.ranges)] + msg.ranges[0:l2]
-        nb_fliter_pass = 1
+        if dist = numpy.linalg.norm(self.goal - self.position) < 0.5:
+            # Because the lidar is oriented backward on the racecar, 
+            # if we want the middle value of the ranges to be forward:
+            l2 = len(msg.ranges)/2;
+            ranges = msg.ranges[l2:len(msg.ranges)] + msg.ranges[0:l2]
+            nb_fliter_pass = 1
 
-        # Smooth ranges
-        for i in range(nb_fliter_pass):
-            for i, p in enumerate(ranges[(len(ranges)//4):(len(ranges)*3//4)]):
-                idx = i + len(ranges)//4
-                p = 0.4 * p + 0.2 * (ranges[idx+1] + ranges[idx-1]) + 0.1 * (ranges[idx+2] + ranges[idx-2])
+            # Smooth ranges
+            for i in range(nb_fliter_pass):
+                for i, p in enumerate(ranges[(len(ranges)//4):(len(ranges)*3//4)]):
+                    idx = i + len(ranges)//4
+                    p = 0.4 * p + 0.2 * (ranges[idx+1] + ranges[idx-1]) + 0.1 * (ranges[idx+2] + ranges[idx-2])
 
-        # Use only front values
-        ranges = ranges[len(ranges)//4 : len(ranges)*3//4]
+            # Use only front values
+            ranges = ranges[len(ranges)//4 : len(ranges)*3//4]
 
-        # Find furthest direction
-        dir_idx = ranges.index(max(ranges))
-        steering_dir = self.idx_to_steering(dir_idx, len(ranges))
+            # Find furthest direction
+            dir_idx = ranges.index(max(ranges))
+            steering_dir = self.idx_to_steering(dir_idx, len(ranges))
 
-        # Find closest obstacle
-        dir_idx = ranges.index(min(ranges))
-        steering_obs = -self.idx_to_steering_inv(dir_idx, len(ranges))
+            # Find closest obstacle
+            dir_idx = ranges.index(min(ranges))
+            steering_obs = -self.idx_to_steering_inv(dir_idx, len(ranges))
 
-        steering = 0.6* steering_dir + 0.4 * steering_obs
-        
-        twist = Twist()
-        twist.linear.x = self.max_speed
-        twist.angular.z = steering
-           
-        self.cmd_vel_pub.publish(twist)
+            steering = 0.6* steering_dir + 0.4 * steering_obs
+            
+            twist = Twist()
+            twist.linear.x = self.max_speed
+            twist.angular.z = steering
+               
+            self.cmd_vel_pub.publish(twist)
+
+        else:
+            self.cmd_vel_pub.publish(Twist())
         
     def odom_callback(self, msg):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        z = msg.pose.pose.position.z
+        self.position = np.array([x, y, z])
         rospy.loginfo("Current speed = %f m/s", msg.twist.twist.linear.x)
 
 def main():
