c6bc0310d2333177cecef5f3cf0fd0f68b32e394
simonchamorro <simon.chamorro@usherbrooke.ca>
2019-12-03 16:48:50 -0500

    added u turn behaviour

--- racecar_control/scripts/path_following.py
+++ racecar_control/scripts/path_following.py
@@ -16,13 +16,15 @@ class PathFollowing:
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self.scan_sub = rospy.Subscriber('scan', LaserScan, self.scan_callback, queue_size=1)
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_callback, queue_size=1)
+        self.goal_tolerance = 1
         self.position = np.array([0, 0, 0])
+        self.start_pos = None
         self.goal = np.array([1, 1, 1])
+        self.u_turn = False
+        self.reached_pos = False
+        self.done = False
 
-    def scan_callback(self, msg):
-
-        #if dist == numpy.linalg.norm(self.goal - self.position) < 0.5:
-
+    def go_forward(self, msg):
         # Because the lidar is oriented backward on the racecar, 
         # if we want the middle value of the ranges to be forward:
         l2 = len(msg.ranges)/2;
@@ -56,16 +58,47 @@ class PathFollowing:
             
         self.cmd_vel_pub.publish(twist)
 
-    # else:
-    #     self.cmd_vel_pub.publish(Twist())
+    def do_u_turn(self, msg):
+        pass
+        # TODO
 
+    def reached_goal(self, goal):
+        return np.linalg.norm(self.position, goal) < self.goal_tolerance
+
+    def scan_callback(self, msg):
+
+        go_forward(msg)
+
+        # Uncomment once u_turn is implemented
+
+
+        # if self.u_turn and not self.done:
+        #     self.do_u_turn(msg)
+
+        # elif not self.reached_pos and not self.done:
+        #     self.go_forward()
+        #     if self.reached_goal(self.goal):
+        #         self.reached_pos = True
+        #         self.u_turn = True
+
+        # elif self.reached_pos and not self.done:
+        #     self.go_forward()
+        #     if self.reached_goal(self.start_pos):
+        #         self.done = True
+
+        # else:
+        #     self.cmd_vel_pub.publish(Twist())
+        #     pass
         
     def odom_callback(self, msg):
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
         z = msg.pose.pose.position.z
         self.position = np.array([x, y, z])
-        #rospy.loginfo("Current speed = %f m/s", msg.twist.twist.linear.x)
+        if self.start_pos is None:
+            self.start_pos = self.position
+       
+        # rospy.loginfo("Current speed = %f m/s", msg.twist.twist.linear.x)
 
 def main():
     rospy.init_node('path_following')
